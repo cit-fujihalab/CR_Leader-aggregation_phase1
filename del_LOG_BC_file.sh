db1803f30ebe8d487d509c9528e924dee4bb8aa9
yanagihara <tras.sector6@gmail.com>
2022-11-14 16:32:54 +0900

    1114

--- del_LOG_BC_file.sh
+++ del_LOG_BC_file.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 
-find APP/LOG/ -maxdepth 1 -type f -name "*.log" -delete
+find APP/logging/ -maxdepth 1 -type f -name "*.log" -delete
 echo 'APP/LOG/内の.logファイルの全削除を実行'
 find -maxdepth 1 -type f -name "*.json" -delete
 echo '.json(BC)ファイルの全削除を実行'
